ca02bb08f3cfcff56548c05706ca7ba80fdb44d6
bhaskarbhimana2002 <bhaskarbhimana2002@gmail.com>
2024-12-15 21:57:57 +0530

    first commit

---
+++ src/constants/index.js
@@ -0,0 +1,36 @@
+import project1 from "../assets/projects/project-1.jpg";
+import project2 from "../assets/projects/project-2.jpg";
+import project3 from "../assets/projects/project-3.jpg";
+import project4 from "../assets/projects/project-4.jpg";
+
+export const HERO_CONTENT = `I am a passionate and dedicated fresher with a strong foundation in both frontend and backend technologies. My technical knowledge spans HTML, CSS, and JavaScript,React js for building responsive and interactive user interfaces, and I am skilled in Spring Boot, JPA, JDBC, and SQL for developing scalable, efficient server-side applications. With a understanding of Core Java, I am able to work seamlessly across the full stack.`;
+
+export const ABOUT_TEXT = `I am a  full-stack developer with   both frontend and backend technologies. On the frontend, I specialize in HTML, CSS, JavaScript, and React.js, while on the backend, I work with Spring Boot, JDBC, and JPA to build robust and scalable applications,I enjoy solving complex problems and creating seamless user experiences, leveraging my expertise to deliver high-quality solutions in every project I undertake,My goal is to continuously evolve as a developer.`;
+
+export const EXPERIENCES = [
+  {
+    year: "2024 - Present",
+    role: "Java Full Stack Trainee",
+    company: "Excel R.",
+    description: `i am as a trainee learned  both frontend,backend  and  developed interactive web pages and  server side application to run backend and  connect with  maden interactive websites and colloborated with our team to make project successful , to define project  with colloborated stake holder of our project requirements and timelines .`,
+    technologies: ["React js","Html", "Css",  "Javascrit","Springboot","Java","Mysql","Maven"],
+  },
+  
+];
+
+export const PROJECTS = [
+  {
+    title: "E-Commerce Website",
+    image: project1,
+    description:
+      "A fully functional e-commerce website with features like product listing, shopping cart, and user authentication.",
+    technologies: ["HTML", "CSS", "React", "Node.js", "MongoDB"],
+  },
+    
+];
+
+export const CONTACT = {
+  address: "Sri nivas hoestel,dhobi ghat rd,officers colony,madhapur,hyderabad,telangana,500081 ",
+  phoneNo: "+91 8500341594 ",
+  email: "bhaskarbhimana2002@gmail.com",
+};
